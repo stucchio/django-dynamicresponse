e0e6f373fdee921a02ce634e6a3f2e0a8268ada7
Chris Stucchio <stucchio@gmail.com>
2011-07-26 21:09:10 +0530

    Created serialized version of listview.

--- dynamicresponse/genericviews.py
+++ dynamicresponse/genericviews.py
@@ -1,8 +1,18 @@
 from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.list import MultipleObjectMixin
 from django.views.generic.base import View
-from response import SerializeObject
+from response import SerializeObject, CR_NOT_FOUND
 
 class SerializeDetailView(SingleObjectMixin, View):
     def get(self, request, **kwargs):
         return SerializeObject(context=self.get_object())
 
+
+class SerializeListView(MultipleObjectMixin, View):
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        allow_empty = self.get_allow_empty()
+        if not allow_empty and len(self.object_list) == 0:
+            raise Serialize(context = { "error" : _(u"Empty list and '%(class_name)s.allow_empty' is False.") % {'class_name': self.__class__.__name__} },
+                            status = CR_NOT_FOUND)
+        return SerializeObject(self.object_list)
