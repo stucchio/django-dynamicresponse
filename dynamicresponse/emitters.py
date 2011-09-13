9669de764f9534fe78e58a1f9662e668fe220be0
Chris Stucchio <stucchio@gmail.com>
2011-09-13 10:04:39 +0530

    Now serialize_fields can be either a set/list, or a method which returns one.

--- dynamicresponse/emitters.py
+++ dynamicresponse/emitters.py
@@ -43,18 +43,18 @@ class Emitter(object):
     ])
 
     def __init__(self, payload, typemapper, handler, fields=(), anonymous=True):
-        
+
         self.typemapper = typemapper
         self.data = payload
         self.handler = handler
         self.fields = fields
         self.anonymous = anonymous
-        
+
         if isinstance(self.data, Exception):
             raise
-    
+
     def method_fields(self, handler, fields):
-        
+
         if not handler:
             return {}
 
@@ -66,13 +66,13 @@ class Emitter(object):
                 ret[field] = t
 
         return ret
-    
+
     def construct(self):
         """
         Recursively serialize a lot of types, and
         in cases where it doesn't recognize the type,
         it will fall back to Django's `smart_unicode`.
-        
+
         Returns `dict`.
         """
 
@@ -80,9 +80,9 @@ class Emitter(object):
             """
             Dispatch, all types are routed through here.
             """
-            
+
             ret = None
-            
+
             if isinstance(thing, QuerySet):
                 ret = _qs(thing, fields=fields)
             elif isinstance(thing, Page):
@@ -113,52 +113,56 @@ class Emitter(object):
             """
             Foreign keys.
             """
-            
+
             return _any(getattr(data, field.name))
-        
+
         def _related(data, fields=()):
             """
             Foreign keys.
             """
-            
+
             return [ _model(m, fields) for m in data.iterator() ]
-        
+
         def _m2m(data, field, fields=()):
             """
             Many to many (re-route to `_model`.)
             """
-            
+
             return [ _model(m, fields) for m in getattr(data, field.name).iterator() ]
-        
+
         def _model(data, fields=()):
             """
             Models. Will respect the `fields` and/or
             `exclude` on the handler (see `typemapper`.)
             """
-            
+
             ret = { }
             handler=None
-            
+
             # Does the model implement get_serialization_fields() or serialize_fields()?
             # We should only serialize these fields.
             if hasattr(data, 'get_serialization_fields'):
-                fields = set(data.get_serialization_fields())
+                fields = set(data.get_serialization_fields)
             if hasattr(data, 'serialize_fields'):
+                fields = data.serialize_fields
+
+            #If the fields is a list or set, just use it as it is.
+            if hasattr(fields, '__call__'): #But if it is callable, call it.
                 fields = set(data.serialize_fields())
-                    
+
             # Is the model a user instance?
             # Ensure that only core (non-sensitive fields) are serialized
             if isinstance(data, User):
                 fields = ('id', 'email', 'first_name')
-                
+
             # Should we explicitly serialize specific fields?
             if fields:
-                
+
                 v = lambda f: getattr(data, f.attname)
 
                 get_fields = set(fields)
                 met_fields = self.method_fields(handler, get_fields)
-                           
+
                 # Serialize normal fields
                 for f in data._meta.local_fields:
                     if f.serialize and not any([ p in met_fields for p in [ f.attname, f.name ]]):
@@ -170,14 +174,14 @@ class Emitter(object):
                             if f.attname[:-3] in get_fields:
                                 ret[f.name] = _fk(data, f)
                                 get_fields.remove(f.name)
-                
+
                 # Serialize many-to-many fields
                 for mf in data._meta.many_to_many:
                     if mf.serialize and mf.attname not in met_fields:
                         if mf.attname in get_fields:
                             ret[mf.name] = _m2m(data, mf)
                             get_fields.remove(mf.name)
-                
+
                 # Try to get the remainder of fields
                 for maybe_field in get_fields:
                     if isinstance(maybe_field, (list, tuple)):
@@ -199,7 +203,7 @@ class Emitter(object):
                         # using different names.
                         ret[maybe_field] = _any(met_fields[maybe_field](data))
 
-                    else:    
+                    else:
                         maybe = getattr(data, maybe_field, None)
                         if maybe:
                             if callable(maybe):
@@ -211,48 +215,48 @@ class Emitter(object):
                             ret[maybe_field] = _any(maybe)
 
             else:
-                
+
                 for f in data._meta.fields:
                     ret[f.attname] = _any(getattr(data, f.attname))
-                
+
                 fields = dir(data.__class__) + ret.keys()
                 add_ons = [k for k in dir(data) if k not in fields]
-                
+
                 for k in add_ons:
                     if not k.startswith('_'):
                         ret[k] = _any(getattr(data, k))
-            
+
             return ret
-        
+
         def _qs(data, fields=()):
             """
             Querysets.
             """
-            
+
             return [ _any(v, fields) for v in data ]
-                
+
         def _list(data, fields=()):
             """
             Lists.
             """
-            
+
             return [ _any(v, fields) for v in data ]
-            
+
         def _dict(data, fields=()):
             """
             Dictionaries.
             """
-            
+
             return dict([ (k, _any(v, fields)) for k, v in data.iteritems() ])
-            
+
         # Kickstart the seralizin'.
         return _any(self.data, self.fields)
-    
+
     def in_typemapper(self, model, anonymous):
         for klass, (km, is_anon) in self.typemapper.iteritems():
             if model is km and is_anon is anonymous:
                 return klass
-        
+
     def render(self):
         """
         This super emitter does not implement `render`,
@@ -270,6 +274,6 @@ class JSONEmitter(Emitter):
         indent = 0
         if settings.DEBUG:
             indent = 4
-        
+
         seria = simplejson.dumps(self.construct(), cls=DateTimeAwareJSONEncoder, ensure_ascii=False, indent=indent)
         return seria
